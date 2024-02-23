6153f70d98808a13fb28a0816ea35e585433d805
sadid369 <sadid.jones@gmail.com>
2024-02-23 12:31:01 +0600

    rivan riverpod tutorial @ 1:14

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Riverpod',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
